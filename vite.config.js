c166a3f55e6242b72eb8cad9657ffd887d7e1396
rahulatmiya <rahulatmiya@gmail.com>
2025-01-14 15:49:05 +0530

    docker file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server : {
+    host: true, // This binds Vite to all available network interfaces
+    port: 5173, // Optional: Customize port if needed
+  },
 })
